70ca3d376620130a3983370c882d074888528f39
Aditya Kasralkar <78255411+adityakasralkar@users.noreply.github.com>
2022-07-16 01:51:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> GITHUB USER API </h1>
         <UserContainer />
       </header>
     </div>
